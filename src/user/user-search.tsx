71b56e54f1b1a6c33bc10840932823292c7bc091
Leon Coto <leoncoto@gmail.com>
2021-02-25 23:54:40 +0000

    more wip

--- src/user/user-search.tsx
+++ src/user/user-search.tsx
@@ -1,82 +1,83 @@
-import { Button, InputGroup, NonIdealState } from "@blueprintjs/core";
+import { Button, Card, InputGroup, NonIdealState } from "@blueprintjs/core";
 import React, { useEffect, useRef, useState } from "react";
 import { SectionLayout } from "../layout/section-layout";
 import { useSearchUsers } from "../octokit/search-user";
 
 export const UserSearch = () => {
-  const [query, setQuery] = useState("");
-  const [input, setInput] = useState("");
+  const [q, setQuery] = useState("john");
+  const [value, setValue] = useState("john");
 
-  const { data, remove, refetch } = useSearchUsers(
-    { q: query, per_page: 2 },
-    { enabled: !!query, staleTime: 0 }
-  );
+  const { data, remove, refetch } = useSearchUsers({ q }, { enabled: !!q });
 
   const inputRef = useRef<HTMLInputElement | null>(null);
 
   useEffect(() => {
-    query && refetch();
-  }, [query, refetch]);
-
-  const controls = (
-    <>
-      {input && (
-        <Button
-          icon="cross"
-          onClick={() => {
-            setQuery("");
-            setInput("");
-            remove();
-            inputRef.current?.focus();
-          }}
-          minimal
-        />
-      )}
-      <Button disabled={!input} intent="primary" type="submit" tabIndex={0}>
-        Search
-      </Button>
-    </>
-  );
+    console.log(q);
+    q && refetch();
+  }, [q, refetch]);
 
   const header = (
     <form
       onSubmit={(e) => {
         e.preventDefault();
-        setQuery(input);
+        setQuery(value);
       }}
     >
       <InputGroup
         placeholder="Search users"
-        onChange={(e) => setInput(e.target.value)}
-        {...{ rightElement: controls, inputRef, value: input }}
+        onChange={(e) => setValue(e.target.value)}
+        inputRef={inputRef}
+        value={value}
+        rightElement={
+          <Button
+            onClick={() => {
+              setQuery("");
+              setValue("");
+              remove();
+              inputRef.current?.focus();
+            }}
+            disabled={!value}
+            icon="cross"
+            minimal
+          />
+        }
       />
     </form>
   );
 
   return (
     <SectionLayout header={header}>
-      {data && data?.data.total_count ? (
-        <>
-          <pre>Total Count: {data?.data.total_count}</pre>
-          <pre>Incomplete Results: {data?.data.incomplete_results}</pre>
-          <pre>{JSON.stringify(data?.data.items, null, 2)}</pre>
-        </>
-      ) : (
-        <NonIdealState
-          icon="search"
-          title={query ? "No search results" : "User search"}
-          description={
-            query ? (
-              <>
-                Your search didn't match any user.
-                <br /> Try something different."
-              </>
-            ) : (
-              "Search users by username or name."
-            )
-          }
-        />
-      )}
+      {!q && <SearchPrompt />}
+      {data?.data.total_count === 0 && <NoResults />}
+      {data?.data.total_count &&
+        data.data.items.map(({ id, login, html_url, avatar_url }) => (
+          <li key={id}>
+            <a href={html_url} target="blank">
+              {login}
+            </a>
+          </li>
+        ))}
     </SectionLayout>
   );
 };
+
+const NoResults = () => (
+  <NonIdealState
+    icon="search"
+    title="No search results"
+    description={
+      <>
+        Your search didn't match any user.
+        <br /> Try something different."
+      </>
+    }
+  />
+);
+
+const SearchPrompt = () => (
+  <NonIdealState
+    icon="search"
+    title="User search"
+    description="Search users by username or name."
+  />
+);
