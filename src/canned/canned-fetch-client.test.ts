a27fd274e84d41ac7e748d2df698d49457d645dc
Leon Coto <leoncoto@gmail.com>
2021-03-21 18:34:17 +0000

    implement canned fetch client

--- src/canned/canned-fetch-client.test.ts
+++ src/canned/canned-fetch-client.test.ts
@@ -1,14 +1,12 @@
-import { cannedFetchClient } from "./canned-fetch-client";
+import { cannedFetchClient, defaultCannedFetchClient } from "./canned-fetch-client";
 
 // only testing content-type=application/json
 describe("canned fetch client", () => {
   it("200 should be ok", () => {
-    const sendRequest = cannedFetchClient({
-      fetchFn: async () => {
-        const body = JSON.stringify({ ok: true });
-        const init = { status: 200 };
-        return new Response(body, init);
-      },
+    const sendRequest = defaultCannedFetchClient(async () => {
+      const body = JSON.stringify({ ok: true });
+      const init = { status: 200 };
+      return new Response(body, init);
     });
     // I think this might be fine, since we can't use any param unless
     // also,the call to the client will be done internally, so we can ensure
@@ -17,13 +15,13 @@ describe("canned fetch client", () => {
   });
 
   it("400 should throw", () => {
-    const sendRequest = cannedFetchClient({
-      fetchFn: async () =>
+    const sendRequest = defaultCannedFetchClient(
+      async () =>
         new Response(JSON.stringify({ message: "failure" }), {
           status: 400,
           statusText: "Bad Request",
-        }),
-    });
+        })
+    );
 
     const error = new Error("Bad Request");
     error.name = "400";
@@ -32,10 +30,54 @@ describe("canned fetch client", () => {
   });
 
   it("non json responses should throw", () => {
-    const sendRequest = cannedFetchClient({
-      fetchFn: async () => new Response("this will not parse", { status: 200 }),
-    });
+    const sendRequest = defaultCannedFetchClient(
+      async () => new Response("this will not parse", { status: 200 })
+    );
 
     return expect(sendRequest({})).rejects.toBeTruthy();
+  });
+
+  it("should parse custom errors", () => {
+    interface ErrorResponse {
+      name: string;
+      errors: string[];
+    }
+
+    class RequestError extends Error {
+      constructor(
+        message: string,
+        public readonly status: number,
+        name: string,
+        public readonly errors: string[]
+      ) {
+        super(message);
+        this.name = name;
+      }
+    }
+    const errorResponse: ErrorResponse = {
+      name: "custom error",
+      errors: ["error 1", "error 2"],
+    };
+
+    const requestError = new RequestError(
+      "Bad Request",
+      400,
+      errorResponse.name,
+      errorResponse.errors
+    );
+
+    const sendRequest = cannedFetchClient(
+      (status, statusText, response: ErrorResponse) => {
+        return new RequestError(statusText, status, response.name, response.errors);
+      }
+    )(
+      async () =>
+        new Response(JSON.stringify(errorResponse), {
+          status: 400,
+          statusText: "Bad Request",
+        })
+    );
+
+    return expect(sendRequest({})).rejects.toThrowError(requestError);
   });
 });
--- src/canned/canned-fetch-client.ts
+++ src/canned/canned-fetch-client.ts
@@ -2,22 +2,26 @@ export type FetchClient<TParams = unknown, TResult = unknown> = (
   params: TParams
 ) => Promise<TResult>;
 
-export interface FetchClientOptions<TParams = unknown, TResult = unknown> {
-  fetchFn: FetchClient<TParams, Response>;
-  parseHTTPError?: FetchClient<Response, TResult>;
-}
-
-export const cannedFetchClient = <TResult = unknown, TParams = unknown>(
-  options: FetchClientOptions<TParams, TResult>
+export const cannedFetchClient = (
+  parseHTTPError?: (status: number, statusText: string, response: any) => Error
+) => <TResult = unknown, TParams = unknown>(
+  fetchFn: FetchClient<TParams, Response>
 ): FetchClient<TParams, TResult> => async (params) => {
-  const response = await options.fetchFn(params);
+  const response = await fetchFn(params);
   const result = await response.json();
 
   if (response.ok) {
-    return result;
+    return result as TResult;
   } else {
-    const error = new Error(response.statusText);
-    error.name = response.status.toString();
-    throw error;
+    if (parseHTTPError) {
+      const error = parseHTTPError(response.status, response.statusText, response);
+      throw error;
+    } else {
+      const error = new Error(response.statusText);
+      error.name = response.status.toString();
+      throw error;
+    }
   }
 };
+
+export const defaultCannedFetchClient = cannedFetchClient();
