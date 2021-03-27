eeae563207d3fd66642cb5e501f4e12356575502
Leon Coto <leoncoto@gmail.com>
2021-03-27 18:17:22 +0000

    add tests for canned query functions

--- src/canned/canned-query-function.test.ts
+++ src/canned/canned-query-function.test.ts
@@ -1,3 +1,64 @@
+import { QueryFunction } from "react-query";
 import { cannedQueryFunction } from "./canned-query-function";
+import { cannedResponseMapper } from "./canned-response-mapper";
 
-describe("canned query function", () => {});
+const failsQueryFn: QueryFunction<boolean> = () => {
+  throw new Error("boom!");
+};
+
+const succeedsQueryFn: QueryFunction<boolean> = () => true;
+
+const identityMapper = cannedResponseMapper({ mapResponse: (x) => x });
+
+const fallbackMapper = cannedResponseMapper({
+  mapResponse: (x) => x,
+  mapError: () => true,
+});
+
+const alwaysFailsMapper = cannedResponseMapper({
+  mapResponse: () => {
+    throw new Error("boom!");
+  },
+});
+
+describe("canned query function", () => {
+  it("failures should fail by default", () => {
+    const cannedQueryFn = cannedQueryFunction({
+      queryFn: failsQueryFn,
+      mapper: identityMapper,
+    });
+
+    return expect(cannedQueryFn({ queryKey: {} })).rejects.toThrowError(
+      new Error("boom!")
+    );
+  });
+
+  it("failures should recover with fallbacks", () => {
+    const cannedQueryFn = cannedQueryFunction({
+      queryFn: failsQueryFn,
+      mapper: fallbackMapper,
+    });
+
+    return expect(cannedQueryFn({ queryKey: {} })).resolves.toEqual(true);
+  });
+
+  it("successes should succeed by default", () => {
+    const cannedQueryFn = cannedQueryFunction({
+      queryFn: succeedsQueryFn,
+      mapper: identityMapper,
+    });
+
+    return expect(cannedQueryFn({ queryKey: {} })).resolves.toEqual(true);
+  });
+
+  it("successes can be mapped to errors", () => {
+    const cannedQueryFn = cannedQueryFunction({
+      queryFn: succeedsQueryFn,
+      mapper: alwaysFailsMapper,
+    });
+
+    return expect(cannedQueryFn({ queryKey: {} })).rejects.toThrowError(
+      new Error("boom!")
+    );
+  });
+});
